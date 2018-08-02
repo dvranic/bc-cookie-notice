448500a08df9b684f35533e92823a665fe9999d3
Dejan RV <dvranic@bettercollective.com>
2018-08-02 15:24:25 +0200

    Added Hanning's script

--- js/bc-cookie-notice.js
+++ js/bc-cookie-notice.js
@@ -97,7 +97,66 @@
     }
 
     function cookieDisable() {
-        console.log('disable cookies');
+        /**
+         * Hanning's script
+         * @param cname
+         * @returns {string}
+         */
+        //Cookie check
+        function getCookieConsent(cname) {
+            var name = cname + "=";
+            var decodedCookie = decodeURIComponent(document.cookie);
+            var ca = decodedCookie.split(';');
+            for(var i = 0; i <ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0) == ' ') {
+                    c = c.substring(1);
+                }
+                if (c.indexOf(name) == 0) {
+                    return c.substring(name.length, c.length);
+                }
+            }
+            return "";
+        }
+
+        if (getCookieConsent('cookieconsent_status') == 'deny'){
+            console.log('Cookie rejected');
+            var analyticalCookies = ['_ga','_gid','_gat','AMP_TOKEN','__utma','__utmt','__utmb','__utmc','__utmz','__utmv','__utmx','__utmxx','_gaexp','_eventqueue','_first_pageview','_jsuid','_custom_data_username','_referrer_og','clicky_userdata','heatmaps_g2g_10','_hjClosedSurveyInvites','_hjDonePolls','_hjMinimizedPolls','_hjDoneTestersWidgets','_hjMinimizedTestersWidgets','_hjIncludedInSample','_vis_opt_exp_EXPERIMENT_ID_goal_GOAL_ID','_vis_opt_test_cookie','_vis_opt_exp_EXPERIMENT_ID_combi','_vis_opt_exp_EXPERIMENT_ID_exclude','_vis_opt_exp_EXPERIMENT_ID_split','_vis_opt_s','_vis_opt_out','_vwo_uuid'];
+
+            var getAllCookieNames = function(){
+                var pairs = document.cookie.split(";");
+                var cookies = [];
+                for (var i=0; i<pairs.length; i++){
+                    var pair = pairs[i].split("=");
+                    cookies.push(pair[0].trim());
+                }
+                return cookies;
+            };
+
+            function intersect(a, b) {
+                var t;
+                if (b.length > a.length) t = b, b = a, a = t; // indexOf to loop over shorter
+                return a
+                    .filter(function (e) {
+                        return b.indexOf(e) > -1;
+                    })
+                    .filter(function (e, i, c) { // extra step to remove duplicates
+                        return c.indexOf(e) === i;
+                    });
+            }
+
+            var fetchedCookies = getAllCookieNames();
+            var foundAnalyticalCookies = intersect(analyticalCookies, fetchedCookies);
+            console.log('Found following analytical cookies on site:');
+            console.log(foundAnalyticalCookies);
+            var host = (window.location.host).replace(/^(https?:\/\/)?(www\.)?/,'');
+            console.log(host);
+            foundAnalyticalCookies.forEach(function(entry) {
+                document.cookie = entry + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/; domain=.'+host;
+                document.cookie = entry + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/; domain=.'+window.location.hostname;
+                document.cookie = entry + '=;expires=Thu, 01 Jan 1970 00:00:00 GMT; path=/; domain='+window.location.hostname;
+            });
+        }
     }
 
     window.addEventListener("load", function(){
